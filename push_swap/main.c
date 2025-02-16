808d7c95bc246ae09acd17f60c710296c8413e47
alejandro <alejandro1996rd@gmail.com>
2025-02-16 17:26:24 +0100

    acortado main

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,29 +6,15 @@ int main(int argc, char **argv)
     t_stack *b;
 
     if (argc < 2)
-        return (0);
-        
-    if (!init_stack(&a) || !init_stack(&b))
-    {
-        write(2, "Error\n", 6);
-        return (1);
-    }
-    
-    if (!process_arguments(argc, argv, a))
-    {
-        write(2, "Error\n", 6);
-        free_stack(a);  // Liberamos a incluso si está parcialmente construido
-        free_stack(b);  // Liberamos b aunque esté vacío
-        return (1);
-    }
-
-    if (is_sorted(a))
+        return (0);   
+    if (!init_stack(&a) || !init_stack(&b) || \
+        !process_arguments(argc, argv, a) || is_sorted(a))
     {
         free_stack(a);
         free_stack(b);
-        return (0);
+        write(2, "Error\n", 6);
+        return (1);
     }
-
     if (stack_size(a) == 2)
         swap(a, 'a');
     else if (stack_size(a) == 3)
@@ -37,7 +23,6 @@ int main(int argc, char **argv)
         sort_five(a, b);
     else
         big_sort(&a, &b);
-
     free_stack(a);
     free_stack(b);
     return (0);
